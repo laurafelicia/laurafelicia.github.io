c62b54b999c05a32e06e5ab0b61cc1eb56861306
laurafelicia <laurafelicia008@gmail.com>
2023-05-08 23:30:19 -0400

    added final project

--- module-2/joystick-button/joystick-button.ino
+++ module-2/joystick-button/joystick-button.ino
@@ -1,4 +1,4 @@
-int xyzPins[] = {27, 26, 2};   //x,y,z pins
+int xyzPins[] = {25, 26, 2};   //x,y,z pins
 void setup() {
   Serial.begin(115200);
   // pinMode(xyzPins[2], INPUT_PULLUP);  //z axis is a button.
@@ -8,10 +8,6 @@ void loop() {
   int xVal = analogRead(xyzPins[0]);
   int yVal = analogRead(xyzPins[1]);
   int zVal = digitalRead(xyzPins[2]);
-  Serial.printf("X,Y,Z: %d,\t%d,\t%d\n", xVal, yVal, zVal);
-  // Serial.printf(zVal);
-  int buttonVal = digitalRead(22);
-  Serial.print(buttonVal);
-  Serial.print('\n');
+  Serial.print(xVal, DEC);
   delay(500);
 }
